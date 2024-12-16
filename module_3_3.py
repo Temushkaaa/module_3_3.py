abc0aa989cdcc5cb79c4fae71378f717f47ec670
Temushkaaa <puhatema@gmail.com>
2024-12-16 17:14:51 +0300

    module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,12 +1,12 @@
 def print_params(a = 1, b = 'строка', c = True):
     print(a, b, c)
-# print_params(b = 25)
-# print_params(c = [1,2,3])
+print_params(b = 25)
+print_params(c = [1,2,3])
 
 values_list = [305, False, 'winter']
 values_dict = {'a': 52, 'b': True, 'c': 'mouse'}
-# print_params(*values_list)
-# print_params(**values_dict)
+print_params(*values_list)
+print_params(**values_dict)
 
 values_list_2 = ['music', 92]
 print_params(*values_list_2, 42)
